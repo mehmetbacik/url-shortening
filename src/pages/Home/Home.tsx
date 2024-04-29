fead4276d5da1f58efde4c18c28d0badd37b7d80
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-04-29 22:32:04 +0300

    Update
    Updated for Header.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { Header } from "../../components/Header";
 
 const Home: React.FC = () => {
   return (
-    <>Home</>
+    <div>
+      <Header />
+      <div className="container mx-auto py-8">
+        <h2 className="text-2xl font-bold mb-4">Welcome to URL Shortening</h2>
+        <p className="text-gray-600">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          vestibulum ex ut felis imperdiet, nec fermentum turpis volutpat.
+          Vestibulum ac lorem eget justo tristique suscipit non et metus. Sed
+          fermentum ultrices felis.
+        </p>
+      </div>
+    </div>
   );
 };
 
