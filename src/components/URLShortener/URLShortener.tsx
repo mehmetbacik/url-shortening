2fae58b3d544375da07e8f62b2cd7739e87eb3a3
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-04-30 22:41:01 +0300

    Update
    Updated for URLShortener.

--- src/components/URLShortener/URLShortener.tsx
+++ src/components/URLShortener/URLShortener.tsx
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const URLShortener: React.FC = () => {
+  const [originalUrl, setOriginalUrl] = useState("");
+  const [shortenedUrl, setShortenedUrl] = useState("");
+  const [error, setError] = useState("");
+
+  const handleFormSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("https://cleanuri.com/api/v1/shorten", {
+        url: encodeURIComponent(originalUrl),
+      });
+      setShortenedUrl(response.data.result_url);
+      setError("");
+    } catch (error) {
+      setError("Failed to shorten URL. Please try again.");
+    }
+  };
+
   return (
-    <>URLShortener</>
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          value={originalUrl}
+          onChange={(e) => setOriginalUrl(e.target.value)}
+          placeholder="Shorten a link here..."
+        />
+        <button type="submit">Shorten It!</button>
+      </form>
+      {shortenedUrl && (
+        <div>
+          <p>Shortened URL: {shortenedUrl}</p>
+          <button onClick={() => navigator.clipboard.writeText(shortenedUrl)}>
+            Copy
+          </button>
+        </div>
+      )}
+      {error && <p>{error}</p>}
+    </div>
   );
 };
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Header } from "../../components/Header";
 import { Hero } from "../../components/Hero";
+import { URLShortener } from "../../components/URLShortener";
 
 const Home: React.FC = () => {
   return (
     <div>
       <Header />
       <Hero />
+      <URLShortener />
     </div>
   );
 };
