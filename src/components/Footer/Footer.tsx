c83bb43f63b5ce42ae13af1c200b3a71f0fc01dc
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-05-02 21:57:32 +0300

    Update
    Updated for footer.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -14,25 +14,73 @@ const Footer: React.FC = () => {
             <Link to="/" className="text-gray-300 hover:text-white px-4">
               Logo
             </Link>
-            <nav>
-              <ul className="flex space-x-4">
-                <li>
-                  <Link to="#" className="text-gray-300 hover:text-white">
-                    Privacy Policy
-                  </Link>
-                </li>
-                <li>
-                  <Link to="#" className="text-gray-300 hover:text-white">
-                    Privacy Policy
-                  </Link>
-                </li>
-                <li>
-                  <Link to="#" className="text-gray-300 hover:text-white">
-                    Privacy Policy
-                  </Link>
-                </li>
-              </ul>
-            </nav>
+            <div className="flex gap-5">
+              <nav>
+                <h3 className="text-gray-300 font-semibold mb-4 text-start">Features</h3>
+                <ul className="flex flex-col items-start">
+                  <li>
+                    <Link to="#" className="text-gray-300 hover:text-white">
+                      Link Shortening
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="#" className="text-gray-300 hover:text-white">
+                      Branded Links
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="#" className="text-gray-300 hover:text-white">
+                      Analytics
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+              <nav>
+                <h3 className="text-gray-300 font-semibold mb-4 text-start">Resource</h3>
+                <ul className="flex flex-col items-start">
+                  <li>
+                    <Link to="#" className="text-gray-300 hover:text-white">
+                      Blog
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="#" className="text-gray-300 hover:text-white">
+                      Developers
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="#" className="text-gray-300 hover:text-white">
+                      Support
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+              <nav>
+                <h3 className="text-gray-300 font-semibold mb-4 text-start">Company</h3>
+                <ul className="flex flex-col items-start">
+                  <li>
+                    <Link to="#" className="text-gray-300 hover:text-white">
+                      About
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="#" className="text-gray-300 hover:text-white">
+                      Our Team
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="#" className="text-gray-300 hover:text-white">
+                      Careers
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="#" className="text-gray-300 hover:text-white">
+                      Contact
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
           </div>
           <div>
             <Link to="#" className="text-gray-300 hover:text-white">
